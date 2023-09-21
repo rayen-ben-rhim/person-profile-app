0b9ab3da7612831f9130826277ed012287ef719f
rayen ben rhim <139109664+rayen-ben-rhim@users.noreply.github.com>
2023-09-21 19:08:00 +0100

    this is the end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: 'rayen ben rhim',
+        bio: 'A passionate React developer',
+        imgSrc: 'https://placekitten.com/200/200',
+        profession: 'Software Engineer',
+      },
+      show: false,
+      mountTime: new Date(),
+    };
+  }
+
+  componentDidMount() {
+    // Set up an interval to update the mountTime every second
+    this.interval = setInterval(() => {
+      this.setState({ mountTime: new Date() });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    // Clear the interval when the component is unmounted
+    clearInterval(this.interval);
+  }
+
+  toggleProfile = () => {
+    this.setState((prevState) => ({
+      show: !prevState.show,
+    }));
+  };
+
+  render() {
+    const { fullName, bio, imgSrc, profession } = this.state.person;
+    const { show, mountTime } = this.state;
+
+    return (
+      <div>
+        <h1>Person Profile App</h1>
+        <button onClick={this.toggleProfile}>
+          {show ? 'Hide Profile' : 'Show Profile'}
+        </button>
+
+        {show && (
+          <div>
+            <img src={imgSrc} alt={fullName} />
+            <p>Name: {fullName}</p>
+            <p>Bio: {bio}</p>
+            <p>Profession: {profession}</p>
+          </div>
+        )}
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Time interval since last component mount: {this.calculateMountTime(mountTime)}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
+
+  calculateMountTime = (mountTime) => {
+    const currentTime = new Date();
+    const timeDifference = Math.floor((currentTime - mountTime) / 1000);
+    return `${timeDifference} seconds`;
+  };
 }
 
 export default App;
+
